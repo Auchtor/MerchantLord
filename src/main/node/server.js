a9e28d66b7d3d30c82b413beb0b4edd70b8257d2
Matthew Blanski <auchtor@gmail.com>
2019-11-22 21:29:02 -0600

    Added in framework for request body

--- src/main/node/server.js
+++ src/main/node/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
+
 const htmlPort = 3000;
 
 app.get('/', function (request, response) {
+  //req.body.searchstring
+  //TODO: database request
   response.render('index');
 })
 
-app.listen(3000, function () {
+app.listen(htmlPort, function () {
   console.log('Example app listening on port {0}!'.replace('{0}',htmlPort));
 })
